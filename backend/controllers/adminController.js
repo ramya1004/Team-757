b62779b62dc9db32701b328c8885eea86453c425
Ramya <ramyabk104@gmail.com>
2025-03-27 21:59:22 +0530

    third commit

---
+++ backend/controllers/adminController.js
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+import appointmentModel from "../models/appointmentModel.js";
+import doctorModel from "../models/doctorModel.js";
+import bcrypt from "bcrypt";
+import validator from "validator";
+import { v2 as cloudinary } from "cloudinary";
+import userModel from "../models/userModel.js";
+import careTakerModel from "../models/careTakerModel.js";
+
+// API for admin login
+const loginAdmin = async (req, res) => {
+    try {
+
+        const { email, password } = req.body
+        console.log(email);
+
+        if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
+            const token = jwt.sign(email + password, process.env.JWT_SECRET)
+            res.json({ success: true, token })
+        } else {
+            res.json({ success: false, message: "Invalid credentials" })
+        }
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+
+}
---
+++ backend/models/careTakerModel.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const careTakerSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    userId: { type: String, default: "" },
+    docId: { type: String, default: "" },
+    image: { type: String, required: true },
+    speciality: { type: String, required: true },
+    degree: { type: String, required: true },
+    experience: { type: String, required: true },
+    about: { type: String, required: true },
+    available: { type: Boolean, default: true },
+    fees: { type: Number, required: true },
+    slots_booked: { type: Object, default: {} },
+    address: { type: Object, required: true },
+    date: { type: Number, required: true },
+}, { minimize: false })
+
+const careTakerModel = mongoose.models.careTaker || mongoose.model("careTaker", careTakerSchema);
+export default careTakerModel;
